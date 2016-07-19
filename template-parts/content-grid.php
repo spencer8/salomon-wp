fe546791b94b25f3d444eb22d45c26eaa082baa5
spencer8 <specer@team-eight.com>
2016-07-19 09:36:57 -0400

    initial

---
+++ template-parts/content-grid.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template part for displaying content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="entry-content">
+		<?php
+
+			echo '<a href="'. get_the_permalink() .'">'. get_the_title() . '</a>';
+
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+				get_the_title()
+			) );
+
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
